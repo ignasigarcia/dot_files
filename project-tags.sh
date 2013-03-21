e8e3ca19de00521c5f0da7b306fa6d510368642a
Ignacio <ignacio_garcia-font@ipcmedia.com>
2013-03-21 14:53:39 +0000

    .ctags file added

--- project-tags.sh
+++ project-tags.sh
@@ -6,6 +6,6 @@ for D in "${PERSONALDEV}"/*
 do
   if [ -d "$D" ]; then
     cd $D
-    touch tags && rm tags && ctags -R --languages=php . > /dev/null 2>&1
+    touch tags && rm tags && ctags . > /dev/null 2>&1
   fi
 done
