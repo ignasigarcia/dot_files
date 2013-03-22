f2516c191ca4d4618fe3ca758b87c7c04b93f3ee
Ignacio <ignacio_garcia-font@ipcmedia.com>
2013-03-22 10:41:01 +0000

    ctags for php updated

--- project-tags.sh
+++ project-tags.sh
@@ -6,6 +6,6 @@ for D in "${PERSONALDEV}"/*
 do
   if [ -d "$D" ]; then
     cd $D
-    touch tags && rm tags && ctags . > /dev/null 2>&1
+    touch tags && rm tags && /home/igarcia/bin/ctags . > /dev/null 2>&1
   fi
 done
