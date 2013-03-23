01e7f7ffc5b17b14459e295864ef897a92675276
ignasigarcia <tasillo@gmail.com>
2013-03-23 11:55:25 +0000

    Bashrc for Macbook, projects-tags updated to make it platform agnostic and gitignore for vim vundle

--- project-tags.sh
+++ project-tags.sh
@@ -1,11 +1,9 @@
 #!/bin/bash
 
-PERSONALDEV="/sites/personaldev/igarcia"
-
 for D in "${PERSONALDEV}"/*
 do
   if [ -d "$D" ]; then
     cd $D
-    touch tags && rm tags && /home/igarcia/bin/ctags . > /dev/null 2>&1
+    touch tags && rm tags && ${CTAGS} . > /dev/null 2>&1
   fi
 done
